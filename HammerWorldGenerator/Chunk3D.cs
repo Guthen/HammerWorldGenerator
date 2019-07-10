368dcb4c6180a02c981a0fd436b772b68cf4ac86
Guthen <33220603+Guthen@users.noreply.github.com>
2019-07-10 20:01:33 +0200

    WIP

--- HammerWorldGenerator/Chunk3D.cs
+++ HammerWorldGenerator/Chunk3D.cs
@@ -2,7 +2,7 @@
 /*
 	MIT License
 
-	Copyright (c) 2019 Guthen
+	Copyright (c) 2019 Gu{
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
@@ -22,20 +22,21 @@
 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 	SOFTWARE.
 */
+using System;
 
 public class Chunk2D
 {
 
-    private int seed = 0;
-    private int cellSize = 8;
-    private int chunkW = 600;
-    private int chunkH = 800;
-    private int surfaceY = 30;
-    private int minSurfaceY = chunkH / cellSize - 15;
-    private int maxSurfaceY = 5;
-    private int[] fillIDs = { 1, 2 };
+    static int seed = 0;
+    static int cellSize = 8;
+    static int chunkW = 600;
+    static int chunkH = 800;
+    static int surfaceY = 30;
+    static int minSurfaceY = chunkH / cellSize - 15;
+    static int maxSurfaceY = 5;
+    static int[] fillIDs = { 1, 2 };
 
-    var random = new Random(seed);
+    Random random = new Random(seed);
 
     /*
 	    --  > get config
@@ -93,168 +94,155 @@ public class Chunk2D
 
     //  > Create Empty Chunk
     public int createEmptyChunk() {
-
-        int[] _chunk = {};
-        for (y = 0; y <= chunkH / cellSize; y++) { 
-            _chunk[y] = {};
-            for (x = 0; x <= chunkW / cellSize, x++) {
-                _chunk[y][x] = 0;
-            };
-        };
+        int[,] _chunk = {};
+        for ( int y = 0; y <= chunkH / cellSize; y++ ) {
+            _chunk[y] = new int[];
+            for ( int x = 0; x <= chunkW / cellSize; x++ ) {
+                _chunk[y,x] = 0;
+            }
+        }
         return _chunk;
     }
 
-    --  > Get SurfaceY Position of the '_chunk' by given '_x' 
-    function getSurfaceY(_chunk, _x )
-	    if _x< 0 then return end
-	    for y, yv in pairs(_chunk ) do
-		    for x, xv in pairs(yv ) do
-			    if _x == x then
-				    if xv > 0 then return y end
+    //  > Get SurfaceY Position of the '_chunk' by given '_x' 
+    public int getSurfaceY( int[] _chunk, int _x ) {
+        if (_x < 0) return;
+        int y = 0;
+        foreach ( int yv in _chunk ) {
+            int x = 0;
+            foreach ( int xv in yv ) {
+                if ( _x == x ) {
+                    if ( xv > 0 ) return y;
+                }
+                x++;
+            }
+            y++;
+        }
+    }
 
-                end
-            end
+    //  > Create & Generate a chunk by using the '_lastChunk'
+    public int generateChunk( ref int[] _chunk, ref int[] _lastChunk ) { 
 
-        end
-    end
+        void fillChunk() { 
+		    for ( int x = 0; x <= chunkW/cellSize; x++ ) {
+                var y = getSurfaceY(_chunk, x);
 
-    --  > Create & Generate a chunk by using the '_lastChunk'
-    function generateChunk(_chunk, _lastChunk )
+			    if ( y ) {
 
-        local function fillChunk()
-		    for x = 0, chunkW/cellSize do
-			    local y = getSurfaceY(_chunk, x)
+                    var b = 0;
+                    var surface = fillIDs[1];
+				    for ( _y = y+1; H/cellSize; _y++ ) {
+                        _chunk[_y][x] = surface;
+                        b = b + 1;
+		 			    if ( b > 0 && fillIDs[b] ) {
+                            surface = fillIDs[b];
+                        }
+                     }
 
-			    if y then
+                 }
+            }
+        }
 
-                    local b = 0
+	    for ( x = 0; x <= chunkW/cellSize; x++ ) { // generate the surface
 
-                    local surface = fillIDs[2]
-				    for _y = y+1, H/cellSize do 
-	 				    _chunk[_y][x] = surface
-                         b = b + 1
-		 			    if b > 1 and fillIDs[b] then
-                             surface = fillIDs[b]
+            var _y;
+            if (_lastChunk && x == 1) {
+                _y = getSurfaceY(_lastChunk, chunkW / cellSize);
+            }
+            else {
+                _y = getSurfaceY(_chunk, x - 1);
+            }
 
-                         end
-                     end
+		    if ( _y ) {
+                int offset = random.Next(-1, 1); // get the Y offset
+                if (_chunk[_y + offset] && _chunk[_y + offset][x]) { // if it's exists
+                    if (_y + offset < maxSurfaceY | _y + offset > minSurfaceY)
+                    {
+                        offset = 0;
+                    }
+                    _chunk[_y + offset][x] = fillIDs[1];
+                }
+            }
+            else if ( _lastChunk == null ) {
+                _chunk[surfaceY][x] = fillIDs[1];
+            }
 
-                 end
-            end
+        }
 
-        end
+        smoothChunk(_chunk, _lastChunk);
+        fillChunk();
+    }
 
-	    for x = 0, chunkW/cellSize do -- generate the surface
+    //  > Create & Generate a world of 'nChunks' chunks
+    public int generateWorld( int nChunks ) {
+        int[] world = {};
+	    for ( int i = 1; i <= nChunks; i++ ) {
+            world[i] = createEmptyChunk();
 
-            local _y
-		    if _lastChunk and x == 1 then
-                _y = getSurfaceY(_lastChunk, chunkW / cellSize)
-		    else
-			    _y = getSurfaceY(_chunk, x-1 )
+            generateChunk( world[i], world[i - 1] );
+        }
+        return world;
+    }
 
-            end
+    //  > Smooth the chunk
+    public void smoothChunk( ref int[] _chunk, ref int[] _lastChunk ) {
+        for ( int x = 0; x <= chunkW / cellSize; x++) { // smoothness
 
-		    if _y then
+            var activeChunk = _chunk;
 
-                local offset = math.random(-1, 1)-- get the Y offset
-			    if _chunk[_y + offset] and _chunk[_y + offset][x] then -- if it's exists
-				    if _y + offset<maxSurfaceY or _y + offset> minSurfaceY then
-                     offset = 0
+            var y, _y, __y, ___y;
+            y = getSurfaceY(_chunk, x);
 
-                    end
-                    _chunk[_y + offset][x] = fillIDs[1]
-    end
+            if (_lastChunk && _chunk[x - 1] == null) {
+                _y = getSurfaceY(_lastChunk, chunkW / cellSize - 1);
 
-            elseif not _lastChunk then
+                activeChunk = _lastChunk;
+            }
+            else {
+                _y = getSurfaceY(_chunk, x - 1);
+            }
+            if (_lastChunk && _chunk[x - 2] == null) {
+                __y = getSurfaceY(_lastChunk, chunkW / cellSize - 2);
 
-                _chunk[surfaceY][x] = fillIDs[1]
-            end
+                activeChunk = _lastChunk;
+            }
+            else { 
+                __y = getSurfaceY(_chunk, x - 2);
+            }
+            if (_lastChunk && _chunk[x - 3] == null) {
+                ___y = getSurfaceY(_lastChunk, chunkW / cellSize - 3);
 
+                activeChunk = _lastChunk;
+            }
+            else {
+                ___y = getSurfaceY(_chunk, x - 3);
+            }
 
-        end
+		    if ( y && _y && __y ) {
+                if (y == __y && (_y != y) && Abs(y - _y) <= 1 && activeChunk[y]) { // if : '0 - 1 - 0' or : '1 - 0 - 1'
+                    if (_y && y && _y < y) { activeChunk[_y][x - 1] = 0; }
 
-        smoothChunk(_chunk, _lastChunk )
+                    activeChunk[y][x - 1] = 1;
+                }
+                else if ( y == ___y && _y == __y && y != _y ) {
+                    activeChunk[y][x - 1] = 2;
 
-        fillChunk()
-    end
+                    activeChunk[y][x - 2] = 2;
+                }
 
-    --  > Create & Generate a world of 'nChunks' chunks
-    function generateWorld(nChunks )
-
-        local world = { }
-	    for i = 1, nChunks do
-		    world[i] = createEmptyChunk()
-
-            generateChunk(world[i], world[i - 1] )
-
-        end
-	    return world
-    end
-
-    --  > Smooth the chunk
-    function smoothChunk(_chunk, _lastChunk )
-	    for x = 0, chunkW/cellSize do -- smoothness
-
-            local activeChunk = _chunk
-
-            local y, _y, __y, ___y
-            y = getSurfaceY(_chunk, x)
-
-		    if _lastChunk and not _chunk[x - 1] then
-                  _y = getSurfaceY(_lastChunk, chunkW / cellSize - 1)
-
-                activeChunk = _lastChunk
-		    else
-			    _y = getSurfaceY(_chunk, x-1 )
-
-            end
-		    if _lastChunk and not _chunk[x - 2] then
-                  __y = getSurfaceY(_lastChunk, chunkW / cellSize - 2)
-
-                activeChunk = _lastChunk
-		    else
-			    __y = getSurfaceY(_chunk, x-2 )
-
-            end
-		    if _lastChunk and not _chunk[x - 3] then
-                  ___y = getSurfaceY(_lastChunk, chunkW / cellSize - 3)
-
-                activeChunk = _lastChunk
-		    else
-			    ___y = getSurfaceY(_chunk, x-3 )
-
-            end
-
-		    if y and _y and __y then
-			    if y == __y and not(_y == y) and math.abs(y - _y ) <= 1 and activeChunk[y] then -- if : '0 - 1 - 0' or : '1 - 0 - 1'
-				    if _y and y and _y<y then activeChunk[_y][x - 1] = 0 end
-
-                    activeChunk[y][x - 1] = 1
-
-                elseif y == ___y and _y == __y and not y == _y then
-
-                    activeChunk[y][x - 1] = 2
-
-                    activeChunk[y][x - 2] = 2
-
-                end
-
-            end
+            }
 
 
-        end
-    end
+        }
+    }
 
-    -- Smooth all of the world chunks
-    function smoothWorld(world )
-	    for id = 1, #world do -- smoothness
-		    local curChunk = world[id]
+    // Smooth all of the world chunks
+    public void smoothWorld( ref int[] world ) {
+        for ( int id = 1; world.Length; id++ ) { 
+		    int[] curChunk = world[id];
+            int[] lastChunk = world[id - 1];
 
-            local lastChunk = world[id - 1]
-
-
-            smoothChunk(curChunk, lastChunk )
-
-        end
-    end
+            smoothChunk( curChunk, lastChunk );
+        }
+    }
 }
--- HammerWorldGenerator/Form1.cs
+++ HammerWorldGenerator/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace HammerWorldGenerator
 {
@@ -17,39 +18,94 @@ namespace HammerWorldGenerator
             InitializeComponent();
         }
 
-        private void GroupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void ButCompile_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void CkBoxChunkFill_CheckedChanged(object sender, EventArgs e)
         {
-
-        }
-
-        private void Label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Label1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void NumericUpDownBlockHeight_ValueChanged(object sender, EventArgs e)
         {
-
         }
 
         private void NumericUpDownBlockWidth_ValueChanged(object sender, EventArgs e)
         {
+        }
 
+        private void gBoxOptions_Enter(object sender, EventArgs e)
+        {
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ButCompile_Click(object sender, EventArgs e)
+        {
+            Console.Write( "HI" );
+            int world = GenerateWorld( int.Parse( tBoxChunkSeed.Text ), Decimal.ToInt32( numericUpDownBlockSize.Value ), 10, Decimal.ToInt32( nUDownChunkWidth.Value ), Decimal.ToInt32( nUDownChunkHeight.Value ), 15, 30, 5 );
+            foreach ( int c in world ) {
+                Console.WriteLine( c );
+            }
+        }
+  
+        private void butGenerate_Click(object sender, EventArgs e)
+        {
+            string save = "versioninfo\r\n{\r\n\t\"editorversion\" \"400\"\r\n\t\"editorbuild\" \"8261\"\r\n\t\"mapversion\" \"19\"\r\n\t\"formatversion\" \"100\"\r\n\t\"prefab\" \"0\"\r\n}\r\nvisgroups\r\n{\r\n}\r\nviewsettings\r\n{\r\n\t\"bSnapToGrid\" \"1\"\r\n\t\"bShowGrid\" \"1\"\r\n\t\"bShowLogicalGrid\" \"0\"\r\n\t\"nGridSpacing\" \"8\"\r\n\t\"bShow3DGrid\" \"0\"\r\n}\r\nworld\r\n{\r\n\t\"id\" \"1\"\r\n\t\"mapversion\" \"19\"\r\n\t\"classname\" \"worldspawn\"\r\n\t\"detailmaterial\" \"detail/detailsprites\"\r\n\t\"detailvbsp\" \"detail.vbsp\"\r\n\t\"maxpropscreenwidth\" \"-1\"\r\n\t\"skyname\" \"minecraft_sky_\"\r\n";
+
+
+            //save = save + lignes[i] + "\r\n";
+            StreamWriter sw = new StreamWriter(this.tBoxOutputPath.Text);
+            sw.Write(save);
+            sw.Close();
+            MessageBox.Show(this, "Succesfully generated the vmf file ! Saved in " + tBoxOutputPath.Text,
+                                   "Notification", MessageBoxButtons.OK,
+                                   MessageBoxIcon.Information);
+        }
+
+        private int GenerateWorld( int seed, int blockSize, int nChunks, int chunkW, int chunkH, int y, int minY, int maxY ) {
+            Chunk3D chunk3d = new Chunk3D();
+            int[] world = chunk3d.set( seed, blockSize, chunkW, chunkH, y, minY, maxY );
+            world.generateWorld( nChunks );
+
+            return world;
+        }
+
+        private string CreateSkybox(string skyname, bool size)
+        {
+            
+        }
+        
+        // CreateBloc( className, posVec, sizeVec, topMat, otherMat );
+        private string CreateBloc(string bloctype, bool breakable) 
+        {
+            
+            string plane = "\t\t\t\"plane\" \"(";
+            string[] stringSeparators = new string[] { ") (", " ", ")\"" };
+            ligne = ligne.Substring(13);
+
+            NumberFormatInfo provider = new NumberFormatInfo();
+            provider.NumberDecimalSeparator = ".";
+
+            string[] groupede3;
+            groupede3 = ligne.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+            int n = 0;
+
+            foreach (var element in groupede3)
+            {
+                double result;
+                result = Convert.ToDouble(groupede3[n], provider);
+
+                double nombre = Math.Round(result, MidpointRounding.AwayFromZero);
+                groupede3[n] = nombre.ToString();
+                n = n + 1;
+            }
+
+            ligne = plane + groupede3[0] + " " + groupede3[1] + " " + groupede3[2] + ") ";
+            ligne += "(" + groupede3[3] + " " + groupede3[4] + " " + groupede3[5] + ") ";
+            ligne += "(" + groupede3[6] + " " + groupede3[7] + " " + groupede3[8] + ")\"";
+            output2.Text = output2.Text + ligne.Substring(3) + "\r\n";
+
+            return ligne;
         }
     }
 }
